0c041f7eea9ab093aaade715171fc2cda32c15a6
sengokyu <sengokyu+gh@gmail.com>
2017-07-31 10:56:49 +0900

    Polish

--- src/ngx-is-equals-to.js
+++ src/ngx-is-equals-to.js
@@ -1,44 +1,34 @@
-/** 
- * (C) 2014
- * 
- * 
- * 
+/**
+ * AngularJS validator.
+ * Validates the field has same value of another field.
+ *
  */
-(function (angular) {
-    'use strict';
+angular.module('ngxIsEqualsTo', [])
+.directive('ngxIsEqualsTo', ['$parse',
+  function ($parse) {
 
-    /**
-     * AngularJS validator.
-     * Validates the field has same value of another field.
-     *
-     */
-    angular.module('ngxIsEqualsTo', []).directive('ngxIsEqualsTo', [
-        '$parse',
-        function ($parse) {
+    var linkFn = function (scope, element, attrs, ctrl) {
 
-            var linkFn = function (scope, element, attrs, ctrl) {
+      ctrl.$validators.ngxIsEqualsTo = function (modelValue, viewValue) {
+        var another = $parse(attrs.ngxIsEqualsTo)(scope);
 
-                ctrl.$validators.ngxIsEqualsTo = function (modelValue, viewValue) {
-                    var another = $parse(attrs.ngxIsEqualsTo)(scope);
-
-                    if (ctrl.$isEmpty(another.$modelValue) && ctrl.$isEmpty(modelValue)) {
-                        return true;
-                    }
-
-                    return viewValue == another.$viewValue;
-                };
-
-                scope.$watch(attrs.ngxIsEqualsTo + '.$modelValue', function () {
-                    ctrl.$validate();
-                });
-            };
-
-            return {
-                restrict: 'A',
-                require: 'ngModel',
-                link: linkFn
-            };
+        if (ctrl.$isEmpty(another.$modelValue) && ctrl.$isEmpty(modelValue)) {
+          return true;
         }
-    ]);
-})(window.angular);
+
+        return viewValue == another.$viewValue;
+      };
+
+      scope.$watch(attrs.ngxIsEqualsTo + '.$modelValue', function () {
+        ctrl.$validate();
+      });
+    };
+
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: linkFn
+    };
+  }
+]);
 
